99f3a528fca3f33113a36cfb64bf30fdec30bc14
Кирилл Зимников <kirill@zimnikov.ru>
2021-04-14 13:06:33 +0700

    Add Cli tips

--- hzlib/cli/main.cpp
+++ hzlib/cli/main.cpp
@@ -9,6 +9,18 @@ int main(int argc, char* argv[])
 	cli.add({'i', "install"}, "Install service", std::bind(&hz::Service::install, &srvc));
 	cli.add({'u', "uninstall"}, "Uninstall service", std::bind(&hz::Service::uninstall, &srvc));
 	
+	enum Types { DRY_RUN, SYNC_ONLY, FULL_WORK };
+	cli.add({'t', "type", {
+			{ DRY_RUN, "dry", "Just test. No real process." },
+			{ SYNC_ONLY, "sync", "Connect and syncronization. Do not proccess data." },
+			{ FULL_WORK, "full", "Synchronize data and proccess them." },
+	}}, "Process type", [](int type) {
+		std::cout << "Use type: " << type << std::endl;
+		switch (type) {
+		case DRY_RUN: break;
+		}
+	});
+
 	cli.add_rule(PAIR, 'i', 's');
 	cli.add_rule(POSSIBLE, 'i', 's', 'v');
 
@@ -17,7 +29,7 @@ int main(int argc, char* argv[])
 	
 	if (cli.process()) // true if action triggered
 		return false; 
-		
+
 	if (cli.has('r'))
 		return true;
 
