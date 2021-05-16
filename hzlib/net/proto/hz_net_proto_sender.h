4365ffab227c577b1073c78cbdc6e44a92f581f9
Кирилл Зимников <kirill@zimnikov.ru>
2021-05-16 08:33:10 +0700

    begin proto sender

---
+++ hzlib/net/proto/hz_net_proto_sender.h
@@ -0,0 +1,18 @@
+#ifndef HZ_NET_PROTO_SENDER_H
+#define HZ_NET_PROTO_SENDER_H
+
+#include "hz_data_stream.h"
+
+namespace hz {
+namespace Net {
+namespace Proto {
+
+class Sender : public Data_Stream
+{
+};
+
+} // namespace Proto
+} // namespace Net
+} // namespace hz
+
+#endif // HZ_NET_PROTO_SENDER_H
